9f040f1740a2ae6181c81a100c5edd6f2fbb2d3a
Isaque Pinheiro <isaquesp@gmail.com>
2026-04-23 09:18:16 -0300

    feat(test/rest-horse-console/#153): rewrite console to canonical TProviderJanus + TProviderDM pattern
    Replace Net.HTTPClient/Net.URLClient self-test runner with TProviderJanus + TProviderDM
    using FireDAC.ConsoleUI.Wait; add IProvider interface, DataModule, Provider, and DFM.

---
+++ Test/Delphi/Tests/JanusRESTHorseConsole.DataModule.pas
@@ -0,0 +1,29 @@
+unit JanusRESTHorseConsole.DataModule;
+
+interface
+
+uses
+  System.SysUtils, System.Classes,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error,
+  FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys,
+  FireDAC.Phys.SQLite, FireDAC.Phys.SQLiteDef, FireDAC.Stan.ExprFuncs,
+  FireDAC.ConsoleUI.Wait,
+  Data.DB, FireDAC.Comp.Client, FireDAC.Comp.UI;
+
+type
+  TProviderDM = class(TDataModule)
+    FDConnection1: TFDConnection;
+    FDGUIxWaitCursor1: TFDGUIxWaitCursor;
+    FDPhysSQLiteDriverLink1: TFDPhysSQLiteDriverLink;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+implementation
+
+{$R *.dfm}
+
+end.
